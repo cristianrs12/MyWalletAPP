a0f5a6434ca2098632ee1f3c1766023f46bb0b78
David <damunozl@alumnos.unex.es>
2015-10-26 19:12:36 +0100

    Lista 1a

--- app/src/main/java/com/example/cristian/mywallet/GastosActivity.java
+++ app/src/main/java/com/example/cristian/mywallet/GastosActivity.java
@@ -2,17 +2,41 @@ package com.example.cristian.mywallet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GastosActivity extends AppCompatActivity {
+    ArrayList<String> gastosList;
     ListView listV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gastos);
+
+        // Register listView
+        listV=(ListView) findViewById(R.id.listView);
+        gastosList = new ArrayList<String>();
+
+        // Fill list with content
+        gastosList.add("Gasto 1");
+        gastosList.add("Gasto 2");
+        gastosList.add("Gasto 3");
+        gastosList.add("Gasto 4");
+        gastosList.add("Gasto 5");
+        gastosList.add("Gasto 6");
+        gastosList.add("Gasto 7");
+
+        // Create The Adapter with passing ArrayList as 3rd parameter
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, gastosList);
+
+        // Set adapter to listView
+        listV.setAdapter(arrayAdapter);
     }
+
     @Override
     public void onStart() {
         super.onStart();
@@ -36,8 +60,6 @@ public class GastosActivity extends AppCompatActivity {
 
         // TODO:
         listV=(ListView) findViewById(R.id.listView);
-
-
     }
 
     @Override
